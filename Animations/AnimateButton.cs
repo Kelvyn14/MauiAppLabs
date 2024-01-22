b8ff3c31ec28133943e0c672364bd22e8cebab5d
Kelvyn Rosa de Araujo <kelvyn.araujo@totvs.com.br>
2024-01-22 12:10:25 -0300

    Add custom animation to custom button control

---
+++ Animations/AnimateButton.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiAppLabs.Animations;
+
+public class AnimateButton
+{
+  /// <summary>
+  /// Indica se existe um controle executando uma animação
+  /// </summary>
+  public static bool IsAnimate { get; set; }
+
+  /// <summary>
+  /// Executa animação do click do botão ou item da lista.
+  /// </summary>
+  /// <param name="visualElement"></param>
+  public static async Task Execute(VisualElement visualElement)
+  {
+    visualElement.Opacity = 0.8;
+    visualElement.AnchorX = 0.50;
+    visualElement.AnchorY = 0.50;
+    await visualElement.ScaleTo(0.95, 100, Easing.Linear);
+    await Task.Delay(100);
+    await visualElement.ScaleTo(1.0, 100, Easing.Linear);
+    visualElement.Opacity = 1.0;
+  }
+}
---
+++ Controls/CustomButton.cs
@@ -0,0 +1,49 @@
+﻿using MauiAppLabs.Animations;
+
+namespace MauiAppLabs.Controls;
+
+public class CustomButton : Button
+{
+
+  /// <summary>
+  /// Evento disparado quando o botão é clickado
+  /// </summary>
+  new public event EventHandler Clicked
+  {
+    add { _Clicked += value; }
+    remove { _Clicked -= value; }
+  }
+
+  private event EventHandler _Clicked;
+
+  public CustomButton() : base()
+  {
+    base.Clicked += CustomButton_Clicked;
+
+  }
+
+  private async void CustomButton_Clicked(object? sender, EventArgs e)
+  {
+    if (_Clicked != null)
+    {
+      // Verifica se já existe um botão em animação
+      if (!AnimateButton.IsAnimate)
+      {
+        AnimateButton.IsAnimate = true;
+
+        try
+        {
+          // Executa animação do clique do botão
+          await AnimateButton.Execute(this);
+
+          // Dispara evento do botão
+          _Clicked(this, e);
+        }
+        finally
+        {
+          AnimateButton.IsAnimate = false;
+        }
+      }
+    }
+  }
+}
