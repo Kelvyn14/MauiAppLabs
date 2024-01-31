a57386a28d15d4b264fedeea94e6a575600866ea
Kelvyn Rosa de Araujo <kelvyn.araujo@totvs.com.br>
2024-01-31 11:23:07 -0300

    moving files and adjusting namespaces

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
-using System.ComponentModel;
-
-namespace MauiAppLabs.Pages.Carousel;
+namespace MauiAppLabs.Pages;
 
 public partial class LoginPage : ContentPage
 {
